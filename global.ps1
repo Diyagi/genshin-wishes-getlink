ad0fd7a31ffcfe3eae22dd62a06d08875ba1c23a
Diyagi <douglas@diyagi.dev>
2022-01-12 22:03:52 -0300

    Fix indentation Spaces

--- global.ps1
+++ global.ps1
@@ -11,50 +11,50 @@ function doPathExists($p){
         return $test
     }
     catch {
-		Write-Debug $_
-		return $false
+        Write-Debug $_
+        return $false
     }
 }
 
 $path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
 if ((doPathExists($path)) -ne $true) {
-	$path = [System.Environment]::ExpandEnvironmentVariables($logLocationChina);
-	if ((doPathExists($path)) -ne $true) {
-		Write-Host "Genshin folder not found in current User Profile folder." -ForegroundColor Red
+    $path = [System.Environment]::ExpandEnvironmentVariables($logLocationChina);
+    if ((doPathExists($path)) -ne $true) {
+        Write-Host "Genshin folder not found in current User Profile folder." -ForegroundColor Red
 
-		# If Path does not exists, then, check if the current user is Admin
-		if (-not $isUserAdmin) {
-			# If not, inform user and ask for permission to run as Admin
-			Write-Host "This script also detected that the current user is not an Administrator,
-						since Genshin Impact is required to run as Administrator,
-						it is possible that this script will also need Admin Rights to
-						read the files." -ForegroundColor Red
+        # If Path does not exists, then, check if the current user is Admin
+        if (-not $isUserAdmin) {
+            # If not, inform user and ask for permission to run as Admin
+            Write-Host "This script also detected that the current user is not an Administrator,
+                        since Genshin Impact is required to run as Administrator,
+                        it is possible that this script will also need Admin Rights to
+                        read the files." -ForegroundColor Red
 
-			Write-Host "`nThis script will now run with Admin Rights,
-						if you want to continue, press any key,
-						if you want to Abort, press ESC." -ForegroundColor Red
-			$keyCode = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+            Write-Host "`nThis script will now run with Admin Rights,
+                        if you want to continue, press any key,
+                        if you want to Abort, press ESC." -ForegroundColor Red
+            $keyCode = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
-			if ($keyCode.VirtualKeyCode -eq 27) {
-				# If the user dont want to run as Admin, he can press ESC(27) to abort
-				return
-			} else {
-				# To run as admin, the script need to start another Powershell terminal
-				# running it as Admin and run the same script again.
-				$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-				$newProcess.Arguments = $myInvocation.MyCommand.Definition;
-				$newProcess.Verb = "runas";
+            if ($keyCode.VirtualKeyCode -eq 27) {
+                # If the user dont want to run as Admin, he can press ESC(27) to abort
+                return
+            } else {
+                # To run as admin, the script need to start another Powershell terminal
+                # running it as Admin and run the same script again.
+                $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+                $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+                $newProcess.Verb = "runas";
 
-				[System.Diagnostics.Process]::Start($newProcess);
-			}
-		}
-		Write-Host "Make sure to run the game and open the wish history first." -ForegroundColor Red
-	}
+                [System.Diagnostics.Process]::Start($newProcess);
+            }
+        }
+        Write-Host "Make sure to run the game and open the wish history first." -ForegroundColor Red
+    }
 }
 
 if (-Not [System.IO.File]::Exists($path)) {
-	Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
-	return
+    Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
+    return
 }
 
 $logs = Get-Content -Path $path

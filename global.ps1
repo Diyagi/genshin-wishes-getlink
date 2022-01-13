86eba60b101b77d4d1c165b7fec2ea45a58b07d0
Diyagi <douglas@diyagi.dev>
2022-01-12 21:52:36 -0300

    Add elevate to admin if needed

--- global.ps1
+++ global.ps1
@@ -1,49 +1,69 @@
 ﻿$logLocation = "%userprofile%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt";
-$LogLocationChina = "%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt";
+$logLocationChina = "%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt";
 
-function tryTestPath($p){
+# https://stackoverflow.com/a/29434931
+$isUserAdmin = $null -ne (whoami /groups /fo csv | ConvertFrom-Csv | Where-Object { $_.SID -eq "S-1-5-32-544" })
+
+# Check if Path to output_log do exists
+function doPathExists($p){
     try {
         $test = Test-Path -Path $p -ErrorAction Stop
         return $test
     }
     catch {
-        if ($_ -like "*Access is denied*") {
-            Write-Host "No permission to open file, try running PowerShell with Administrator rights." -ForegroundColor Red
-            return "denied"
-        }
+		Write-Debug $_
+		return $false
     }
 }
 
-$path = [System.Environment]::ExpandEnvironmentVariables($logLocation)
-$test = tryTestPath($path)
-if ($test -match "denied") { return; }
-if ($test -ne $true) {
+$path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
+if ((doPathExists($path)) -ne $true) {
+	$path = [System.Environment]::ExpandEnvironmentVariables($logLocationChina);
+	if ((doPathExists($path)) -ne $true) {
+		Write-Host "Genshin folder not found in current User Profile folder." -ForegroundColor Red
 
-    $path = [System.Environment]::ExpandEnvironmentVariables($LogLocationChina)
-    $test = tryTestPath($path)
-    if ($test -match "denied") { return }
-    if ($test -ne $true) {
+		# If Path does not exists, then, check if the current user is Admin
+		if (-not $isUserAdmin) {
+			# If not, inform user and ask for permission to run as Admin
+			Write-Host "This script also detected that the current user is not an Administrator,
+						since Genshin Impact is required to run as Administrator,
+						it is possible that this script will also need Admin Rights to
+						read the files." -ForegroundColor Red
 
-        Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
-        return
-    }
+			Write-Host "`nThis script will now run with Admin Rights,
+						if you want to continue, press any key,
+						if you want to Abort, press ESC." -ForegroundColor Red
+			$keyCode = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+			if ($keyCode.VirtualKeyCode -eq 27) {
+				# If the user dont want to run as Admin, he can press ESC(27) to abort
+				return
+			} else {
+				# To run as admin, the script need to start another Powershell terminal
+				# running it as Admin and run the same script again.
+				$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+				$newProcess.Arguments = $myInvocation.MyCommand.Definition;
+				$newProcess.Verb = "runas";
+
+				[System.Diagnostics.Process]::Start($newProcess);
+			}
+		}
+		Write-Host "Make sure to run the game and open the wish history first." -ForegroundColor Red
+	}
 }
 
-try {
-    $logs = Get-Content -Path $path -ErrorAction Stop
-}
-catch {
-    if ($_ -like "*is denied.*") {
-        Write-Host "No permission to open file, try running PowerShell with Administrator rights." -ForegroundColor Red
-        return
-    }
+if (-Not [System.IO.File]::Exists($path)) {
+	Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
+	return
 }
 
+$logs = Get-Content -Path $path
 $match = $logs -match "^OnGetWebViewPageFinish.*log$"
 if (-Not $match) {
     Write-Host "We cannot find the wish history url! Make sure to open the wish history ingame first!" -ForegroundColor Red
     return
 }
+
 [string] $wishHistoryUrl = $match[$match.count-1] -replace 'OnGetWebViewPageFinish:', ''
 Write-Host $wishHistoryUrl
 Set-Clipboard -Value $wishHistoryUrl
